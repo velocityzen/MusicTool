a337d5eaccccf65611703c7dc94b5f67ea24e30c
velocityzen <velocityzen@gmail.com>
2019-11-29 12:09:05 -0500

    Initial Commit

---
+++ Music Tool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Music Tool
+//
+//  Created by velocityzen on 11/29/19.
+//  Copyright © 2019 velocityzen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
